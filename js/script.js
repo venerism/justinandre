5e7c2494e5bbe3d5e0f7db71a26ce76a3574a3ea
justin veneracion <justin.veneracion11@gmail.com>
2018-04-08 01:20:17 -0400

    adds broken js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-         $(‘nav’).addClass(‘changeColor’)
-      }
-      if ($(this).scrollTop() < 50) {
-         $(‘nav’).removeClass(‘changeColor’)
-      }
-   });
-});
+(function(){
+
+  var link,
+      toggleScrollToTopLink = function() {
+
+          if($("body").scrollTop() > 0 ||
+          $("html").scrollTop() > 0 {
+            link.fadeIn(400);
+          }
+            else{
+              link.fadeOut(400);
+            }
+        };
+
+      $(document).ready(function(){
+
+          link = $(".scroll-to-top-link");
+
+          $(window).scroll(toggleScrollToTopLink);
+
+          toggleScrollToTopLink();
+
+          link.on("click", fuction(){
+              $("body").animate({scrollTop:0});
+              $("html").animate({scrollTop:0});
+
+          })
+
+      });
+
+})();
