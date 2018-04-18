d45825b9843075434cf3ff0f558ad8a9189fdbaf
justin veneracion <justin.veneracion11@gmail.com>
2018-04-18 19:30:03 -0400

    adds js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,5 @@
-(function(){
 
-  var link,
-      toggleScrollToTopLink = function() {
-
-          if($("body").scrollTop() > 0 ||
-          $("html").scrollTop() > 0 {
-            link.fadeIn(400);
-          }
-            else{
-              link.fadeOut(400);
-            }
-        };
-
-      $(document).ready(function(){
-
-          link = $(".scroll-to-top-link");
-
-          $(window).scroll(toggleScrollToTopLink);
-
-          toggleScrollToTopLink();
-
-          link.on("click", fuction(){
-              $("body").animate({scrollTop:0});
-              $("html").animate({scrollTop:0});
-
-          })
-
-      });
-
-})();
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+};
